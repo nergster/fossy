91f45570b5cc56439e7cb30632c423df3085c4ff
Dr. Rubisco <76263371+thedocruby@users.noreply.github.com>
2023-06-03 10:06:08 -0400

    Add `bevyengine`, license headers
    Add license headers.
    Add the Bevy game engine as a dependency. (https://github.com/bevyengine/bevy)
    Use the `pixel-perfect` example from https://github.com/bevyengine/bevy/tree/v0.10.1/examples
    Add `assets` directory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
+/************************************************************************
+ *  Fossy - A FOSS video game experiment.
+ *  Copyright (C) 2023  Nergster
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ************************************************************************/
+
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_startup_system(setup)
+        .add_system(sprite_movement)
+        .run();
+}
+
+#[derive(Component)]
+enum Direction {
+    Left,
+    Right,
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn((
+        SpriteBundle {
+            texture: asset_server.load("textures/icon.png"),
+            transform: Transform::from_xyz(100., 0., 0.),
+            ..default()
+        },
+        Direction::Right,
+    ));
+}
+
+fn sprite_movement(time: Res<Time>, mut sprite_position: Query<(&mut Direction, &mut Transform)>) {
+    for (mut logo, mut transform) in &mut sprite_position {
+        match *logo {
+            Direction::Right => transform.translation.x += 30. * time.delta_seconds(),
+            Direction::Left => transform.translation.x -= 30. * time.delta_seconds(),
+        }
+
+        if transform.translation.x > 200. {
+            *logo = Direction::Left;
+        } else if transform.translation.x < -200. {
+            *logo = Direction::Right;
+        }
+    }
 }
